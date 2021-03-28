2d91425207c55f11d4eb66706e09cb0438ec170e
razielron <razielron@gmail.com>
2021-03-28 20:49:20 +0300

    add fives algorithm

--- ex1/ex1/sort.h
+++ ex1/ex1/sort.h
@@ -7,6 +7,10 @@ namespace ex1
 	class Sort {
 	public:
 		static double insertionSort(double *&num, int size, int index);
+		static void swap(double* num1, double* num2);
+		static void bubbleSort(double** num, int size);
+		static double medianFinder(double*& arr, int n, int size);
+		static double quintileSort(double** num, int size, int index);
 		static void sortMenu(DynamicArr<double> &arr, int i);
 		static void printArr(double *&num, int size);
 		static double selectionSort(double*& Arr, int left, int right, int i);
